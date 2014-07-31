ab5c054dacfefc32787a872ace2a764e3eb8b70c
bgrove1974 <bgrove1974@gmail.com>
2014-07-31 06:03:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+VoterApp::Application.config.secret_token = 'c5ef9d45c903d83dc6f9fbab0a9c52a3d9a161f58839af9e54016b71d674d380e6c829d24ed35e3f2882418a38223d062942a8a8e16509ae49cf66915a870798'
