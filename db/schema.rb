4c545b7f8b183d1ef5c026e3bb6cd43097e0f633
bgrove1974 <bgrove1974@gmail.com>
2014-08-06 06:21:47 -0700

    Create Issues scaffold and rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140802070454) do
+ActiveRecord::Schema.define(version: 20140806132002) do
+
+  create_table "issues", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "address1"
