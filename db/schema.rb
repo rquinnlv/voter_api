edbfc1baa82ba97c055a69d21da587a54f0bef04
bgrove1974 <bgrove1974@gmail.com>
2014-08-07 07:32:19 -0700

    Create tables for election_info and candidates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807135142) do
+ActiveRecord::Schema.define(version: 20140807142942) do
+
+  create_table "candidates", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "election_infos", force: true do |t|
+    t.string   "date"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "elections", force: true do |t|
     t.datetime "created_at"
